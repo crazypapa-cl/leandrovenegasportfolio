47d7d33f02c6aee75ca5e48fd0618c5b855cdaa6
crazypapa-cl <crazypapaa@gmail.com>
2025-10-09 19:53:15 -0300

    mustatis muandi imagenes y videos up

--- src/app/portafolio/Rayandola/mutatis/page.tsx
+++ src/app/portafolio/Rayandola/mutatis/page.tsx
@@ -8,18 +8,26 @@ export default function Proyectos() {
          <h2>Dos dias de puro talento local en valparaiso</h2>
          <p className="text-sm italic">7 y 8 de abril 2023</p>
           <Image src='/portafolio/mutatis/img/mutatis-logo.png' alt='Mutatis Logo' width={500} height={500} priority></Image>
-         <div className="grid grid-cols-3 gap-4 items-center content-center rounded-xl">
+         <div className="sm:grid-cols-2  grid grid-cols-3 gap-4 items-center content-center rounded-xl">
        
-    <Link href={'/portafolio/mutatis/img/babuch-diseño-logo-mutatis.jpg'}><Image className='rounded-xl'src='/portafolio/mutatis/img/babuch-diseño-logo-mutatis.jpg' alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+
         <Image className='rounded-xl'src='/portafolio/mutatis/img/mutatis-festival-valparaiso-afiche-calle-prat.jpg' alt='Mutatis Logo' width={500} height={500}></Image>
+         <Link href={'/portafolio/mutatis/img/poster-mutatis-festival-todos-los-artistas-valparaiso.png'}><Image className='rounded-xl'src='/portafolio/mutatis/img/poster-mutatis-festival-todos-los-artistas-valparaiso.png' alt='Mutatis Logo' width={500} height={500}></Image></Link> 
         <Image className='rounded-xl'src='/portafolio/mutatis/img/mutatis-mutandi-poster-calle-valparaiso-pedro-mont.jpg' alt='Mutatis Logo' width={500} height={500}></Image>
         <Image className='rounded-xl'src='/portafolio/mutatis/img/mutatis-mutandi-festiva-valparaiso-proceso-de-creacion-afiche-serigrafia.jpg' alt='Mutatis Logo' width={500} height={500}></Image>
+        <Link href={'mutatis-mutandi-expo-festival-poster-calle-polanco-valparaiso.jpg'}><Image className='rounded-xl'src='/portafolio/mutatis/img/mutatis-mutandi-expo-festival-poster-calle-polanco-valparaiso.jpg' alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+        <Link href={'/portafolio/mutatis/img/babuch-diseño-logo-mutatis.jpg'}><Image className='rounded-xl'src='/portafolio/mutatis/img/babuch-diseño-logo-mutatis.jpg' alt='Mutatis Logo' width={500} height={500}></Image></Link> 
        </div>
-          <div className="grid gap-8 mt-8">
+          <div className="grid sm:grid-cols-2 gap-8 mt-8">
             <video className='rounded-xl' controls src="/portafolio/mutatis/video/video-promocion-mutatis-mutandi-festival-valparaiso.mp4">
             </video>
             <video className='rounded-xl' controls src="/portafolio/mutatis/video/video-promocion-mutatis-mutandi-festival.mp4">
             </video>
+            <video className='rounded-xl' controls src="/portafolio/mutatis/video/Mutatis-mutandi-expofest-valpaaiso-reel-promo-flow-mila.mp4">
+            </video>
+            <video className='rounded-xl' controls src="/portafolio/mutatis/video/mutatis-expo-fest-reel-promo-nigth-nitty.mp4">
+            </video>
+          
           </div>
     </div>
   );
