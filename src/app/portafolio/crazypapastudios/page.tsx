06eafe26dc94ffad0c6633f294b690c18c4bb7a2
crazypapa-cl <crazypapaa@gmail.com>
2025-10-10 10:49:18 -0300

    Crazyroom Fotos y Videos

---
+++ src/app/portafolio/crazypapastudios/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function Proyectos() {
+  console.log("hola");
+  return (
+    <div className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+          <Image src='/portafolio/crazypapastudios/img/Logo-Crazypapa-Vector.svg' alt='Logo CrazypapaStdudios' width={500} height={500} priority></Image>
+          <h3 className="text-3xl font-sans m-5 font-bold underline decoration-wavy">Studio Creativo</h3>
+         <div className="sm:grid-cols-2  grid grid-cols-3 gap-4 items-center content-center rounded-xl">
+       
+
+        <Image className='rounded-xl'src='/portafolio/mutatis/img/mutatis-festival-valparaiso-afiche-calle-prat.jpg' alt='Mutatis Logo' width={500} height={500}></Image>
+         <Link href={'/portafolio/mutatis/img/poster-mutatis-festival-todos-los-artistas-valparaiso.png'}><Image className='rounded-xl'src='/portafolio/mutatis/img/poster-mutatis-festival-todos-los-artistas-valparaiso.png' alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+        <Image className='rounded-xl'src='/portafolio/mutatis/img/mutatis-mutandi-poster-calle-valparaiso-pedro-mont.jpg' alt='Mutatis Logo' width={500} height={500}></Image>
+        <Image className='rounded-xl'src='/portafolio/mutatis/img/mutatis-mutandi-festiva-valparaiso-proceso-de-creacion-afiche-serigrafia.jpg' alt='Mutatis Logo' width={500} height={500}></Image>
+        <Link href={'mutatis-mutandi-expo-festival-poster-calle-polanco-valparaiso.jpg'}><Image className='rounded-xl'src='/portafolio/mutatis/img/mutatis-mutandi-expo-festival-poster-calle-polanco-valparaiso.jpg' alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+        <Link href={'/portafolio/mutatis/img/babuch-diseño-logo-mutatis.jpg'}><Image className='rounded-xl'src='/portafolio/mutatis/img/babuch-diseño-logo-mutatis.jpg' alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+       </div>
+          <div className="grid sm:grid-cols-2 gap-8 mt-8">
+            <video className='rounded-xl' controls src="/portafolio/mutatis/video/video-promocion-mutatis-mutandi-festival-valparaiso.mp4">
+            </video>
+            <video className='rounded-xl' controls src="/portafolio/mutatis/video/video-promocion-mutatis-mutandi-festival.mp4">
+            </video>
+            <video className='rounded-xl' controls src="/portafolio/mutatis/video/Mutatis-mutandi-expofest-valpaaiso-reel-promo-flow-mila.mp4">
+            </video>
+            <video className='rounded-xl' controls src="/portafolio/mutatis/video/mutatis-expo-fest-reel-promo-nigth-nitty.mp4">
+            </video>
+          
+          </div>
+    </div>
+  );
+}
--- src/app/portafolio/crazyroom/page.tsx
+++ src/app/portafolio/crazyroom/page.tsx
@@ -2,9 +2,38 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Proyectos() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-white text-3xl font-bold">Proyectos</h1>
-         <Link href='/'><li>CRAZY ROOM</li></Link>
+    <div className="flex flex-col h-min-screen content-center ">
+     <h1 className="text-white text-3xl font-bold">CRAZYROOM</h1>
+       <Link href={'/portafolio/crazyroom/img/LOGO-CRAZY-ROOM-REC-LIVE-SESSIONS-valparaiso.png'}>
+    <Image className='rounded-xl'src='/portafolio/crazyroom/img/LOGO-CRAZY-ROOM-REC-LIVE-SESSIONS-valparaiso.png' 
+    alt='Mutatis Logo' width={500} height={500}></Image></Link>
+
+
+     <div  className=" grid grid-cols-1 sm:grid-cols-2 justify-center gap-4">
+    <Link href={'/portafolio/crazyroom/img/Flayer-Crazyroom-Rec-Live-Session-valparaiso-jose-Manojo.png'}>
+    <Image className='rounded-xl'src='/portafolio/crazyroom/img/Flayer-Crazyroom-Rec-Live-Session-valparaiso-jose-Manojo.png' 
+    alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+    <Link href={'/portafolio/crazyroom/img/LOGO-CRAZY-ROOM-REC-LIVE-SESSIONS-valparaiso.png'}>
+    <Image className='rounded-xl'src='/portafolio/crazyroom/img/HELLO-KETA-FLAYER-MAKETA-V2.png' 
+    alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+    <Link href={'/portafolio/crazyroom/img/LOGO-CRAZY-ROOM-REC-LIVE-SESSIONS-valparaiso.png'}>
+    <Image className='rounded-xl'src='/portafolio/crazyroom/img/Flayer-Crazy-Room-Master-Gonazalo-Barrera.png' 
+    alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+    <Link href={'/portafolio/crazyroom/img/LOGO-CRAZY-ROOM-REC-LIVE-SESSIONS-valparaiso.png'}>
+    <Image className='rounded-xl'src='/portafolio/crazyroom/img/FlayerCrazyRoom-Gonzalo-barrera-Valparaiso.png' 
+    alt='Mutatis Logo' width={500} height={500}></Image></Link> 
+     </div>
+<div className="grid grid-cols-2 gap-4 content-center justify-center items-center mt-8 ">
+  <video src="/portafolio/crazyroom/video/HELLO-KETTA-VERSION-REEL-01.mp4" controls width="350" height="350">
+
+  </video>
+  <video src="/portafolio/crazyroom/video/Master-Historia-CrazyRoom-Valpo.mp4" controls width="350" height="350">
+
+  </video>
+  <video src="/portafolio/crazyroom/video/Reel-Promo-REEL.mp4" controls width="350" height="350">
+
+  </video>
+</div>
     </div>
   );
 }
