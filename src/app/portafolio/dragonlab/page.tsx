ec18f6a15d87bcaf7bc40d9e44d4f2fbe0e7441e
crazypapa-cl <crazypapaa@gmail.com>
2025-10-13 00:57:10 -0300

    Nuevo material

--- src/app/portafolio/dragonlab/page.tsx
+++ src/app/portafolio/dragonlab/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Proyectos() {
   return (
     <div className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-white text-3xl font-bold">Mutatis music Festival</h1>
+      <h1 className="text-white text-3xl font-bold">Dragon lab</h1>
          <h2>Dos dias de puro talento local en valparaiso</h2>
          <p className="text-sm italic">7 y 8 de abril 2023</p>
         
